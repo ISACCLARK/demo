ef1ddf6c802fd07d43df4c4353c5fd211bcecd45
ISACCLARK <112521943+ISACCLARK@users.noreply.github.com>
2022-08-31 00:13:45 -0600

    vamos viendo
    de nuevo

--- mi-archivo.js
+++ mi-archivo.js
@@ -2,4 +2,4 @@ alert("mensaje editado");
 console.log("Este es un nuevo mensaje");
 console.log("Es una prueba para github de escritorio");
 console.log("ultimo cambio por testear");
-console.log("testeo de prueba para ver errores");
+console.log("testeo de prueba para ver errores una vez mas");
